c53139a388b8619e1acb2e6af38c5b8d31185a97
qasem <qasem@qasem.home>
2025-10-26 16:34:40 -0400

    node

--- router/users.js
+++ router/users.js
@@ -45,11 +45,54 @@ router.put('/:id', verifyTokenAndAuthorization, asyncHandler(async (req, res) =>
 
 router.get('/', verifyTokenAndAdmin, asyncHandler(async (req, res) => {
     const users = await User.find().select('-password');
-
     res.status(200).json(users);
 }));
 
 
+/**
+ * @desc get all users
+ * @route /api/users
+ * @method GET
+ * @access Private (Admin only)
+ * 
+ */
 
+router.get('/', verifyTokenAndAdmin, asyncHandler(async (req, res) => {
+    const users = await User.find().select('-password');
+    res.status(200).json(users);
+}));
+
+
+/**
+ * @desc get user by id
+ * @route /api/users/:id
+ * @method GET
+ * @access Private (Admin only)
+ * 
+ */
+
+router.get('/:id', verifyTokenAndAuthorization, asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+}));
+
+/**
+ * @desc delete user by id
+ * @route /api/users/:id
+ * @method DELETE
+ * @access Private (Admin only)
+ * 
+ */
+
+router.delete('/:id', verifyTokenAndAuthorization, asyncHandler(async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).send({ message: 'User deleted successfully' });
+}));
 
 module.exports = router;
